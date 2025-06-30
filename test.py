a64ce769eb8d7b60adab70689bba7af7b3fe7546
santoshtiwari41 <santoshhh066@gmail.com>
2025-06-30 21:10:33 +0545

    fix:api key exposed

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAXWIcGQsd6HYKH3BZYgkzBXG9i9-mOlMw")
-
-models = genai.list_models()
-
-for model in models:
-    print(f"{model.name} -> {model.supported_generation_methods}")
